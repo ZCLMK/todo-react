1c7c52aeff226210c08de938f248fab6252112b7
zac <zacharie.NTC@gmail.com>
2018-07-20 23:45:23 +0800

    with local Storage!

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import deleteBtn from './images/deleteBtn.png'
+import CSSTransitionGroup from 'react-transition-group/CSSTransitionGroup';
 
 class TodoItems extends Component {
+  constructor(props){
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
   createTasks(item) {
     return (
-      <div id="list-item-div">
-        <li key={item.key}>{item.text}></li>
-        <a href='#'>
+      <div key={item.key} id="list-item-div">
+        
+        <div id="item-title">
+          <p  onClick={() => this.delete(item.key)}>{item.text}</p>
+        </div>
+        
+        <div onClick={() => this.delete(item.key)} id='delete-btn'>
           <img width='26' height='26'id='#delete-img' src={deleteBtn} alt="delete button"/>
-        </a>
+        </div>
+      
       </div>
     )
   }
 
+  delete(key){
+    this.props.delete(key);
+  }
   render() {
     var todoEntries = this.props.entries;
     var listItems = todoEntries.map(this.createTasks)
     return (
-      <ul className='theList'>
-        {listItems}
-      </ul>
+        <CSSTransitionGroup
+          transitionName="task"
+          transitionEnterTimeout={350}
+          transitionLeaveTimeout={300}>
+          {listItems}
+        </CSSTransitionGroup>
+    
     )
   }
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -4,19 +4,36 @@ import './todoList.css';
 
 class TodoList extends React.Component {
   constructor(props){
-    super(props);
-    this.state = {
-      items:[]
+      super(props);
+      this.addItem = this.addItem.bind(this);
+      this.deleteItem = this.deleteItem.bind(this);
+      this.state = {
+        items:[]
+      }
     }
-    this.addItem = this.addItem.bind(this);
-    }
+componentDidMount() {
 
+const storageKeys  = Object.keys(window.localStorage)
+let itemsOnRefresh = []
+storageKeys.forEach((key) =>{
+  let item = {}
+  item["key"] = key
+  item['text'] = window.localStorage.getItem(String(key))
+itemsOnRefresh.push(item)
+})
+
+this.setState({items: itemsOnRefresh})
+}
   addItem(e){
-    if(this._inputElement.value !== ""){
+    if( /[A-Za-z0-9]/.test(this._inputElement.value)){  // string needs to contain at least one number of letter (cap or not)
+
       var newItem = {
         text: this._inputElement.value,
         key: Date.now()
       }
+      localStorage.setItem(String(newItem.key), newItem.text)
+
+            //----------------------------------------------Local storage-----------------------------------------------------
       this.setState((prevState) => {
         return {
           // concat allows us to work with a copy of the 'items' array (immutability of state)
@@ -24,28 +41,41 @@ class TodoList extends React.Component {
         }
       })
       this._inputElement.value = ""
-      console.log(this.state.items)
+      // console.log(this.state.items)
+      console.log(window.localStorage)
       e.preventDefault()
     }
-
   }
+    deleteItem(key) {
+      
+     let filteredItems = this.state.items.filter((item) => item.key !== key)
+     window.localStorage.removeItem(String(key))
+    //  console.log(window.localStorage)
+    this.setState({items: filteredItems})
+      }
+
+  
   render() {
     return(
       <div className="todoListMain">
         <div className="header">
+          <h1> Todo list</h1>
           <form onSubmit={this.addItem}>
             <input 
               type="text" 
               placeholder="Ajouter une tâche..."
               ref={(a) => this._inputElement = a}
               ></input>
-            <button type="submit">+</button>
+            <button type="submit">Créer</button>
           </form>
         </div>
-        <TodoItems entries={this.state.items}/>
+        <div id="the-list">
+        <TodoItems entries={this.state.items}
+                              delete={this.deleteItem}/>
+        </div>
       </div>
     )
-  };
+  }
 }
 
 export default TodoList;
